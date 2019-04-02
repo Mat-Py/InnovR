a81a4cbc98e288dd17738dad18e381642630f64a
Percelay Mathieu <mathieu.percelay@insa-rennes.fr>
2019-04-02 11:42:57 +0200

    C project setup

---
+++ C_reading/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "tree.h"
+
+char id_leaf[N_NODES];
+char f_son[N_FATHERS];
+char nb_coeffs[N_LEAVES] ;
+double coeffs[N_COEFFS];
+
+int main() {
+
+    return 0;
+}
---
+++ C_reading/tree.h
@@ -0,0 +1,9 @@
+#ifndef TREE_H
+#define TREE_H
+
+#define N_LEAVES 31
+#define N_FATHERS 11
+#define N_COEFFS 214
+#define N_NODES N_LEAVES + N_FATHERS
+
+#endif
