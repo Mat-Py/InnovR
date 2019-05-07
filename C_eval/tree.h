6fd09ae843e88c410ccb2f7550ccc28e73c7d3f0
mathieu.percelay <mathieup@mailoo.org>
2019-05-07 10:29:18 +0200

    TestDeg1

--- C_eval/tree.h
+++ C_eval/tree.h
@@ -1,11 +1,11 @@
 #ifndef TREE_H
 #define TREE_H
 
-#define N_LEAVES 52
-#define N_FATHERS 18
-#define N_COEFFS 12168
+#define N_LEAVES 742
+#define N_FATHERS 248
+#define N_COEFFS 798289
 #define N_NODES N_LEAVES + N_FATHERS
-#define MAX_DEGREE 3
+#define MAX_DEGREE 1
 #define NB_BITS (unsigned int)1.585
 
 const int id_leaf[N_NODES];
